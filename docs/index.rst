02b2ced879ae0a43a070ea814d7db1f9e2f5ca67
sunwhale <sunwhale@126.com>
2023-04-13 15:58:54 +0800

    环境初始化。

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. pyfem documentation master file, created by
+   sphinx-quickstart on Thu Apr 13 15:41:09 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to pyfem's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyfem",
+    version="0.0.1",
+    author="Jingyu Sun",
+    author_email="sun.jingyu@outlook.com",
+    description="A finite element package for learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sunwhale/pyfem",
+    project_urls={
+        "Bug Tracker": "https://github.com/sunwhale/pyfem/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.10",
+)
